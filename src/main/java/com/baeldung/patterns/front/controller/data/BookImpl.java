f71222a1db91c09c581cb86a41eb41844efc963a
Deniska Asus <k.denis.vlad@gmail.com>
2020-08-29 11:02:56 +0200

    Класс BookImpl имплемент интерфейса Book

---
+++ src/main/java/com/baeldung/patterns/front/controller/data/BookImpl.java
@@ -0,0 +1,37 @@
+package com.baeldung.patterns.front.controller.data;
+
+public class BookImpl implements Book {
+    private String author;
+    private String title;
+    private Double price;
+
+    @Override
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
